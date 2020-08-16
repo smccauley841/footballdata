1b7a72837241f502958f5e112bee5a448221b617
Stephen McCauley <smccauley841@gmail.com>
2020-08-16 19:42:52 +0100

    http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.football-data.org/',
+  apiUrl: 'https://api.football-data.org/',
   apiToken: '2d7d242b623b4552993877c3ffce1c85'
 };
