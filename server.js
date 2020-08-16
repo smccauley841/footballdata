f4de09817f5b6ad5da90a3ce3ba900d0b90fd2dc
Stephen McCauley <smccauley841@gmail.com>
2020-08-16 19:37:14 +0100

    heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/footballdata'));
+app.use(express.static(__dirname + '/dist/football'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/footballdata/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/football/index.html'));
 });
 
 // default Heroku PORT
